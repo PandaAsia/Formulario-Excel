92d308432a9ae1e9dffc17800b15f888d157a410
Panda <shanaUsumaki@gmail.com>
2024-10-21 10:39:32 -0500

    formulario

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,67 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+//importar la biblioteca
+import * as XLSX from 'xlsx';
+
+const InitalForm={
+  nombre: '',
+  numero:'',
+  cargo: '',
+  dependencia: '',
+  telefono: '',
+  correo:''
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [form, setForm]=useState(InitalForm);
+
+
+  const handleChange=(e)=>{
+    const {name, value}=e.target;
+    
+    setForm({...form, [name]:value});
+  }
+
+  const handleSubmit=(e)=>{
+    e.preventDefault();
+
+    const datos=[{
+      ...form
+    }]
+
+    // Crear un nuevo libro de Excel
+    const wb = XLSX.utils.book_new();
+    const ws = XLSX.utils.json_to_sheet(datos);
+
+    // Agregar hoja al libro
+    XLSX.utils.book_append_sheet(wb, ws, "Datos");
+
+    // Generar archivo Excel
+    XLSX.writeFile(wb, "datos_formulario.xlsx");
+  }
+
+  const handleResaet=()=>{
+    setForm(InitalForm);
+  }
+
+
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <section className='flex flex-col gap-4 justify-center h-screen w-screen items-center '><div>
+      <h1 className='text-3xl font-bold'>FORMULARIO</h1>
+    </div>
+    <form action="" className='flex flex-col w-[500px] gap-4 [&>input]:p-2' onSubmit={handleSubmit}>
+      <input type="text" placeholder='Nombre Completo' name='nombre' value={form.nombre} onChange={handleChange}/>
+      <input type="text" placeholder='DNI' name='numero' value={form.numero} onChange={handleChange}/>
+      <input type="text" placeholder='Cargo'name='cargo' value={form.cargo} onChange={handleChange}/>
+      <input type="text" placeholder='Dependenaica' name='dependencia' value={form.dependencia} onChange={handleChange}/>
+      <input type="text" placeholder='telefono' name='telefono' value={form.telefono} onChange={handleChange}/>
+      <input type="text" placeholder='Correo' name='correo' value={form.correo} onChange={handleChange}/>
+      <input type="submit" value="Enviar"  className='bg-green-700 hover:bg-green-950'/>
+      <input type="reset" value="Limpiar" className='bg-red-700 hover:bg-red-950' onClick={handleResaet}/>
+    </form></section>
+    
     </>
   )
 }
